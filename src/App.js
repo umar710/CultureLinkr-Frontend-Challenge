a1e186297f4fc679fa46cb2f0fb68b44418fc011
umar710 <umarsyed082@gmail.com>
2024-10-24 08:50:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message) {
+      setSuccessMessage(
+        <p className="alert-text">Please fill in all fields.</p>
+      );
+    } else {
+      setSuccessMessage(`${formData.name} Your Form submitted successfully!`);
+      //clear the form after submission
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contact-us-card">
+        <h1 className="heading"> Submit Form</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              className="input"
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="label" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="input"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              className="input"
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
+
+        {successMessage && <p className="success-message">{successMessage}</p>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
